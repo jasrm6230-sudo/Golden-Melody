9506aae40ed6cbb37e05f8ec54d0da7824f1f878
Golden Melody <jasrm6230@gmail.com>
2026-07-24 14:14:30 +0400

    Delete stockfish-worker.js

--- stockfish-worker.js
+++
@@ -1,5 +0,0 @@
-// تحديد المسار الصحيح لملف stockfish.wasm (يحتوي الشبكة العصبية NNUE)
-self.locateFile = (file) => `https://lichess1.org/stockfish/${file}`;
-
-// تحميل كود المحرك من Lichess (خفيف وقوي جداً)
-importScripts('https://lichess1.org/stockfish/stockfish.js');
