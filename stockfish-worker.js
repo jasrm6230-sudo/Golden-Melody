b5960a670d86ffb6e614b67c52f0e85f305e78d2
Golden Melody <jasrm6230@gmail.com>
2026-07-24 14:00:11 +0400

    Update stockfish-worker.js

--- stockfish-worker.js
+++ stockfish-worker.js
@@ -1,1 +1,5 @@
-importScripts('https://cdn.jsdelivr.net/npm/stockfish.wasm@0.10.0/stockfish.js');
+// تحديد المسار الصحيح لملف stockfish.wasm (يحتوي الشبكة العصبية NNUE)
+self.locateFile = (file) => `https://lichess1.org/stockfish/${file}`;
+
+// تحميل كود المحرك من Lichess (خفيف وقوي جداً)
+importScripts('https://lichess1.org/stockfish/stockfish.js');
